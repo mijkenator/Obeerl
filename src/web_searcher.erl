0ae18628919251267c8acdf296febde226234c8d
root <root@localhost.localdomain>
2009-10-18 19:38:25 +0400

    res

--- src/web_searcher.erl
+++ src/web_searcher.erl
@@ -1,14 +1,14 @@
 %%%-------------------------------------------------------------------
 %%% File    : web_searcher.erl
 %%% Author  : mijk_ <mijkenator@gmail.com>
-%%% Description : logger server
+%%% Description : 
 %%%-------------------------------------------------------------------
 -module(web_searcher).
 
 -behaviour(supervisor).
 
 -export([start_link/0]).
--export([init/1]).
+-export([init/1, start_worker/0, start_worker/1]).
 
 
 start_link() ->
@@ -17,16 +17,24 @@ start_link() ->
     
 
 init(_Args) ->
+    io:format("supervisor init ~p ~n", [self()]),
    {ok, {
             {one_for_one, 1, 60},
             [
-             {wsw1, {ws_worker, start_link, []},
+             {wsw1, {ws_worker, start_link, ['Worker1']},
                permanent, 5000, worker, []},
-             {wsw2, {ws_worker, start_link, []},
+             {wsw2, {ws_worker, start_link, ['Worker2']},
                permanent, 5000, worker, []}
             ]
         }
     }.
-
+    
+start_worker() -> start_worker(self()).
+start_worker(Pid) ->
+    MyPid = self(),
+    io:format("supervisor start worker ~p ~n", [MyPid]),
+    supervisor:start_child(Pid,
+        {wsw0, {ws_worker, start_link, ['Worker0']},
+            permanent, 5000, worker, []}).
 
 
--- src/ws_worker.erl
+++ src/ws_worker.erl
@@ -1,8 +1,32 @@
 -module(ws_worker).
--export([start_link/0]).
+-author('mijkenator@gmail.com').
 
-start_link() ->
-    io:format("wsworker started ~n"),
-    mijkutils:sleep(5),
-    io:format("wsworker exited ~n"),
-    ok.
+-export([start_link/1]).
+-export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+
+-behaviour(gen_server).
+
+-record(worker_state, {
+        workername,
+        wokrnumber,
+        configitem}).
+
+start_link(WorkerName) ->
+    io:format("wsworker started ~p ~n", [WorkerName]),
+    gen_server:start_link({local, WorkerName}, ?MODULE, #worker_state{workername = WorkerName}, []).
+    
+init(_Args) ->
+  %gen_server:cast(self(), {accepted, self()}),
+  {ok, 'worker inited'}.
+  
+handle_cast({accepted, _Pid}, State=#worker_state{}) ->
+    io:format("cast !!!! ~n"),
+    {noreply, State};
+handle_cast(_Msg, State) ->
+    {noreply, State}. 
+  
+% These are just here to suppress warnings.
+handle_call(_Msg, _Caller, State) -> {noreply, State}.
+handle_info(_Msg, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
